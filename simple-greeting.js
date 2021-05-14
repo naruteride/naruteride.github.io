a9ef8284074c3fb213c26812ec8395409b2c1119
bangseonghun <slimejam01@gmail.com>
2021-05-15 01:24:46 +0900

    add babel-plugin-wildcard module

--- simple-greeting.js
+++ simple-greeting.js
@@ -1,5 +1,5 @@
 import {customElement, property} from "./node_modules/lit/decorators.js";
-import { html, css, LitElement } from "./node_modules/lit/*";
+import { html, css, LitElement } from "./node_modules/lit";
 // import { LitElement } from "./node_modules/lit-element/lit-element.d.ts"
 // import { html } from "./node_modules/lit-html/lit-html.d.ts"
 // import { css } from "./node_modules/@lit/reactive-element/css-tag.d.ts"
