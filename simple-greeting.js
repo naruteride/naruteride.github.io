07f96eddee32d81501def4d4f1b73a019759d041
bangseonghun <slimejam01@gmail.com>
2021-05-14 23:07:30 +0900

    modify import path in js

--- simple-greeting.js
+++ simple-greeting.js
@@ -1,5 +1,5 @@
-import {customElement, property} from 'lit/decorators.js';
-import { html, css, LitElement } from "lit";
+import {customElement, property} from './node_modules/lit/decorators.js';
+import { html, css, LitElement } from "./node_modules/lit";
 
 export class SimpleGreeting extends LitElement {
   static get styles() {
