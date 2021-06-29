7402bc508694e9536bf0ca1d86e9929ced992e7a
bangseonghun <slimejam01@gmail.com>
2021-06-29 19:25:50 +0900

    Update index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -27,4 +27,7 @@ onload = function() {
   document.querySelector("#map").addEventListener("click", function() {
     alert("맵을 클릭함")
   })
+  document.querySelector("#bottom-drawer>section").addEventListener("click", function() {
+    alert("섹션을 클릭함")
+  })
 }
