9db8c1a406e5fb10e5b500521f173d56cd723c89
bangseonghun <slimejam01@gmail.com>
2021-07-02 04:36:54 +0900

    Update index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -10,7 +10,6 @@ function initMap() {
   });
 }
 
-
 onload = function () {
   let bottomDrawer = document.querySelector("#bottom-drawer");
 
@@ -22,15 +21,16 @@ onload = function () {
     .then(function (html) {
       bottomDrawer.innerHTML = html;
     })
+    .then(function () {
+      let drawerSection1 = bottomDrawer.querySelector("section:nth-child(1)");
+      let drawerSection2 = bottomDrawer.querySelector("section:nth-child(2)");
+
+      drawerSection1.addEventListener("click", function () {
+        alert("섹션 1에 터치를 함");
+      });
+      console.log("코드 합치기 성공");
+    })
     .catch(function (err) {
       console.warn("Bottom drawer fetch went wrong.", err);
     });
-
-  let drawerSection1 = bottomDrawer.querySelector("section:nth-child(1)");
-  let drawerSection2 = bottomDrawer.querySelector("section:nth-child(2)");
-
-  drawerSection1.addEventListener("click", function () {
-    alert("섹션 1에 터치를 함");
-  });
-  console.log("코드 합치기 성공")
 };
