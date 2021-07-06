6f003703a04c42b9fdfe0c361e71d7c02495a13a
bangseonghun <slimejam01@gmail.com>
2021-07-06 14:32:27 +0900

    separate js file

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,5 @@
+import "./bottom-drawer.js";
+
 onload = function () {
   let bottomDrawer = document.querySelector("#bottom-drawer");
 
@@ -9,36 +11,9 @@ onload = function () {
     .then(function (html) {
       bottomDrawer.innerHTML = html;
     })
+    // 크롬 버그 해결
     .then(function () {
-      // 크롬일 경우에만 아래 코드를 통해, 바텀드로워 버그 해결
-      if (!!window.chrome) {
-        let drawerSection1 = bottomDrawer.querySelector("section:nth-child(1)");
-        let drawerSection2 = bottomDrawer.querySelector("section:nth-child(2)");
-
-        // 터치 할 때 .pointer-events-auto가 추가되고 땔 때 지워짐.
-        // 섹션 1
-        drawerSection1.addEventListener("touchstart", function () {
-          bottomDrawer.classList.add("pointer-events-auto");
-          console.log("touch start");
-        });
-        drawerSection1.addEventListener("touchend", function () {
-          setTimeout(() => {
-            bottomDrawer.classList.remove("pointer-events-auto");
-            console.log("touch end");
-          }, 600);
-        });
-        // 섹션 2
-        drawerSection2.addEventListener("touchstart", function () {
-          bottomDrawer.classList.add("pointer-events-auto");
-          console.log("touch start");
-        });
-        drawerSection2.addEventListener("touchend", function () {
-          setTimeout(() => {
-            bottomDrawer.classList.remove("pointer-events-auto");
-            console.log("touch end");
-          }, 600);
-        });
-      }
+      chromeBugFixer();
     })
     .catch(function (err) {
       console.warn("Bottom drawer fetch went wrong.", err);
