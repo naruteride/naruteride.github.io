51a0c125ea629adf856d4978a15180bf89ed9acd
bangseonghun <slimejam01@gmail.com>
2021-07-14 22:46:42 +0900

    Update main.js

--- publicity-page/main.js
+++ publicity-page/main.js
@@ -1,46 +1,40 @@
-onload = function() {
+onload = function () {
+  let markerOnMap = document.querySelector("#marker-on-map");
+  let markerOnMapLocation = markerOnMap.getBoundingClientRect();
 
-    let markerOnMap = document.querySelector("#marker-on-map");
-    let markerOnMapLocation = markerOnMap.getBoundingClientRect();
+  let friends = document.querySelector("#friends");
+  let friendsLocation = friends.getBoundingClientRect();
 
+  let lastSection = document.querySelector("#last-section");
+  let lastSectionLocation = lastSection.getBoundingClientRect();
 
-    let friends = document.querySelector("#friends");
-    let friendsLocation = friends.getBoundingClientRect();
+  let footer = document.querySelector("#footer");
+  let footerLocation = footer.getBoundingClientRect();
 
+  let scrollSnap = document.querySelector(".scroll-snap");
+  scrollSnap.addEventListener("scroll", function (event) {
+    if (scrollSnap.scrollTop >= markerOnMapLocation.top) {
+      for (let li of markerOnMap.querySelectorAll("ol>li")) {
+        li.classList.add("animation-on");
+      }
+      setTimeout(function () {
+        friends.classList.remove("display-none");
+      }, 1000);
+    }
 
-    let lastSection = document.querySelector("#last-section");
-    let lastSectionLocation = lastSection.getBoundingClientRect();
+    if (scrollSnap.scrollTop >= friendsLocation.top) {
+      for (let title of friends.querySelectorAll("h2, h1")) {
+        title.classList.add("animation-on");
+      }
+      setTimeout(function () {
+        lastSection.classList.remove("display-none");
+      }, 1000);
+    }
 
-
-    let footer = document.querySelector("#footer");
-    let footerLocation = footer.getBoundingClientRect();
-
-    
-
-
-
-    let scrollSnap = document.querySelector(".scroll-snap");
-    scrollSnap.addEventListener("scroll", function(event) {
-
-        if(scrollSnap.scrollTop >= markerOnMapLocation.top) {
-            for(let li of markerOnMap.querySelectorAll("ol>li")) {
-                li.classList.add("animation-on");
-            }
-            friends.classList.remove("display-none");
-        }
-
-
-        if(scrollSnap.scrollTop >= friendsLocation.top) {
-            for(let title of friends.querySelectorAll("h2, h1")) {
-                title.classList.add("animation-on");
-            }
-            lastSection.classList.remove("display-none");
-        }
-
-
-        if(scrollSnap.scrollTop >= lastSectionLocation.top) {
-            footer.classList.remove("display-none");
-        }
-    })
-}
-
+    if (scrollSnap.scrollTop >= lastSectionLocation.top) {
+      setTimeout(function () {
+        footer.classList.remove("display-none");
+      }, 100);
+    }
+  });
+};
