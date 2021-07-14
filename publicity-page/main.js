52dabb33812e7fce226c31bca14a0aec587d6621
bangseonghun <slimejam01@gmail.com>
2021-07-14 23:18:54 +0900

    fix scrolling bug

--- publicity-page/main.js
+++ publicity-page/main.js
@@ -1,19 +1,24 @@
 onload = function () {
   let markerOnMap = document.querySelector("#marker-on-map");
   let markerOnMapLocation = markerOnMap.getBoundingClientRect();
+	let didMarkerOnMapIsWork = false;
 
   let friends = document.querySelector("#friends");
   let friendsLocation = friends.getBoundingClientRect();
+	let didfriendsIsWork = false;
 
   let lastSection = document.querySelector("#last-section");
   let lastSectionLocation = lastSection.getBoundingClientRect();
+	let didlastSectionIsWork = false;
 
   let footer = document.querySelector("#footer");
   let footerLocation = footer.getBoundingClientRect();
 
   let scrollSnap = document.querySelector(".scroll-snap");
   scrollSnap.addEventListener("scroll", function (event) {
-    if (scrollSnap.scrollTop >= markerOnMapLocation.top) {
+    if (scrollSnap.scrollTop >= markerOnMapLocation.top && !didMarkerOnMapIsWork) {
+			didMarkerOnMapIsWork = true;
+
       for (let li of markerOnMap.querySelectorAll("ol>li")) {
         li.classList.add("animation-on");
       }
@@ -26,7 +31,9 @@ onload = function () {
       }, 500);
     }
 
-    if (scrollSnap.scrollTop >= markerOnMapLocation.top * 2) {
+    if (scrollSnap.scrollTop >= markerOnMapLocation.top * 2 && !didfriendsIsWork) {
+			didfriendsIsWork = true;
+
       for (let title of friends.querySelectorAll("h2, h1")) {
         title.classList.add("animation-on");
       }
@@ -39,7 +46,8 @@ onload = function () {
       }, 500);
     }
 
-    if (scrollSnap.scrollTop >= markerOnMapLocation.top * 3) {
+    if (scrollSnap.scrollTop >= markerOnMapLocation.top * 3 && !didlastSectionIsWork) {
+			didlastSectionIsWork = true;
 
 			scrollSnap.classList.add("pointer-events-none");
 
