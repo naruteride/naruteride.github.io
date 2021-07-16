6a39e5f42f6b94d12967e3ba696451ac499fb776
bangseonghun <slimejam01@gmail.com>
2021-07-16 14:51:28 +0900

    move chromeBugFixer function into bottom-drawer.js & rename bottom-category to drawer-category

--- public/scripts/bottom-drawer.js
+++ public/scripts/bottom-drawer.js
@@ -4,13 +4,38 @@ export function bottomDrawerEventInit(
   drawerSection2,
   knob
 ) {
-  // if (!!window.chrome) {
-  //   chromeBugFixer(bottomDrawer, drawerSection1, drawerSection2);
-  // }
+  if (!!window.chrome) {
+    chromeBugFixer(bottomDrawer, drawerSection1, drawerSection2);
+  }
 
   knob.addEventListener("click", function () {
     drawerSection2.scrollIntoView({ behavior: "smooth", block: "start" });
   });
 }
 
-
+// 크롬 스크롤 버그 고치는 함수
+function chromeBugFixer(bottomDrawer, drawerSection1, drawerSection2) {
+  // 터치 할 때 .pointer-events-auto가 추가되고, 땔 때 지워짐.
+  // 섹션 1
+  drawerSection1.addEventListener("touchstart", function () {
+    bottomDrawer.classList.add("pointer-events-auto");
+    console.log("touch start");
+  });
+  drawerSection1.addEventListener("touchend", function () {
+    setTimeout(() => {
+      bottomDrawer.classList.remove("pointer-events-auto");
+      console.log("touch end");
+    }, 600);
+  });
+  // 섹션 2
+  drawerSection2.addEventListener("touchstart", function () {
+    bottomDrawer.classList.add("pointer-events-auto");
+    console.log("touch start");
+  });
+  drawerSection2.addEventListener("touchend", function () {
+    setTimeout(() => {
+      bottomDrawer.classList.remove("pointer-events-auto");
+      console.log("touch end");
+    }, 600);
+  });
+}
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -12,7 +12,7 @@ onload = function () {
   bottomDrawer = document.querySelector("#bottom-drawer");
 
   // 게시판 fetch
-  fetch("/views/bottom-category/posts.html")
+  fetch("/views/drawer-category/posts.html")
     .then(function (response) {
       return response.text();
     })
@@ -29,16 +29,13 @@ onload = function () {
     .then(function () {
       // 바텀 드로워 이벤트 초기화
       bottomDrawerEventInit(bottomDrawer, drawerSection1, drawerSection2, knob);
-      if (!!window.chrome) {
-        chromeBugFixer();
-      }
     })
     .catch(function (err) {
       console.warn("posts of bottom drawer fetch went wrong.", err);
     });
 
   // 게시글 fetch
-  fetch("/views/bottom-category/post.html")
+  fetch("/views/drawer-category/post.html")
     .then(function (response) {
       return response.text();
     })
@@ -49,31 +46,3 @@ onload = function () {
       console.warn("post of bottom drawer fetch went wrong.", err);
     });
 };
-
-
-// 크롬 스크롤 버그 고치는 함수
-function chromeBugFixer() {
-  // 터치 할 때 .pointer-events-auto가 추가되고, 땔 때 지워짐.
-  // 섹션 1
-  drawerSection1.addEventListener("touchstart", function () {
-    bottomDrawer.classList.add("pointer-events-auto");
-    console.log("touch start");
-  });
-  drawerSection1.addEventListener("touchend", function () {
-    setTimeout(() => {
-      bottomDrawer.classList.remove("pointer-events-auto");
-      console.log("touch end");
-    }, 600);
-  });
-  // 섹션 2
-  drawerSection2.addEventListener("touchstart", function () {
-    bottomDrawer.classList.add("pointer-events-auto");
-    console.log("touch start");
-  });
-  drawerSection2.addEventListener("touchend", function () {
-    setTimeout(() => {
-      bottomDrawer.classList.remove("pointer-events-auto");
-      console.log("touch end");
-    }, 600);
-  });
-}
