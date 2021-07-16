e5cd0688c29fbe248f5c8f441911cf402b08e748
bangseonghun <slimejam01@gmail.com>
2021-07-16 14:30:45 +0900

    move chromeBugFixer function into index.js

--- public/scripts/bottom-drawer.js
+++ public/scripts/bottom-drawer.js
@@ -4,37 +4,13 @@ export function bottomDrawerEventInit(
   drawerSection2,
   knob
 ) {
-  if (!!window.chrome) {
-    chromeBugFixer(bottomDrawer, drawerSection1, drawerSection2);
-  }
+  // if (!!window.chrome) {
+  //   chromeBugFixer(bottomDrawer, drawerSection1, drawerSection2);
+  // }
 
   knob.addEventListener("click", function () {
     drawerSection2.scrollIntoView({ behavior: "smooth", block: "start" });
   });
 }
 
-function chromeBugFixer(bottomDrawer, drawerSection1, drawerSection2) {
-  // 터치 할 때 .pointer-events-auto가 추가되고, 땔 때 지워짐.
-  // 섹션 1
-  drawerSection1.addEventListener("touchstart", function () {
-    bottomDrawer.classList.add("pointer-events-auto");
-    console.log("touch start");
-  });
-  drawerSection1.addEventListener("touchend", function () {
-    setTimeout(() => {
-      bottomDrawer.classList.remove("pointer-events-auto");
-      console.log("touch end");
-    }, 600);
-  });
-  // 섹션 2
-  drawerSection2.addEventListener("touchstart", function () {
-    bottomDrawer.classList.add("pointer-events-auto");
-    console.log("touch start");
-  });
-  drawerSection2.addEventListener("touchend", function () {
-    setTimeout(() => {
-      bottomDrawer.classList.remove("pointer-events-auto");
-      console.log("touch end");
-    }, 600);
-  });
-}
+
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -29,6 +29,9 @@ onload = function () {
     .then(function () {
       // 바텀 드로워 이벤트 초기화
       bottomDrawerEventInit(bottomDrawer, drawerSection1, drawerSection2, knob);
+      if (!!window.chrome) {
+        chromeBugFixer();
+      }
     })
     .catch(function (err) {
       console.warn("posts of bottom drawer fetch went wrong.", err);
@@ -46,3 +49,31 @@ onload = function () {
       console.warn("post of bottom drawer fetch went wrong.", err);
     });
 };
+
+
+// 크롬 스크롤 버그 고치는 함수
+function chromeBugFixer() {
+  // 터치 할 때 .pointer-events-auto가 추가되고, 땔 때 지워짐.
+  // 섹션 1
+  drawerSection1.addEventListener("touchstart", function () {
+    bottomDrawer.classList.add("pointer-events-auto");
+    console.log("touch start");
+  });
+  drawerSection1.addEventListener("touchend", function () {
+    setTimeout(() => {
+      bottomDrawer.classList.remove("pointer-events-auto");
+      console.log("touch end");
+    }, 600);
+  });
+  // 섹션 2
+  drawerSection2.addEventListener("touchstart", function () {
+    bottomDrawer.classList.add("pointer-events-auto");
+    console.log("touch start");
+  });
+  drawerSection2.addEventListener("touchend", function () {
+    setTimeout(() => {
+      bottomDrawer.classList.remove("pointer-events-auto");
+      console.log("touch end");
+    }, 600);
+  });
+}
