2e4ef0b8be1a8a767bd70e68e6b4e7f2cf629fba
bangseonghun <slimejam01@gmail.com>
2021-08-22 17:33:01 +0900

    activate google map & add write file and write button

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,27 +1,40 @@
-// let map;
-// let myLatLng = { lat: 37.5575312, lng: 126.9222782 }
-// let funckingMarkerPosition = { lat: 37.5574111, lng: 126.9245447 };
+let map;
+let myLatLng = { lat: 37.5575312, lng: 126.9222782 };
+let funckingMarkerPosition = { lat: 37.5574111, lng: 126.9245447 };
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.5575312, lng: 126.9222782 },
+    zoom: 17,
+    disableDefaultUI: true,
+  });
 
-// function initMap() {
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
 
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 37.5575312, lng: 126.9222782 },
-//     zoom: 17,
-//     disableDefaultUI: true,
-//   });
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+  });
+}
 
-//   new google.maps.Marker({
-//     position: myLatLng,
-//     map,
-//     title: "Hello World!",
-//   });
-// }
-
-// function addMarker(markerPosition, title) {
-//   new google.maps.Marker({
-//     position: markerPosition,
-//     map,
-//     title: this.title,
-//   });
-// }
+function addMarker(markerPosition, title) {
+  new google.maps.Marker({
+    position: markerPosition,
+    map,
+    title: this.title,
+  });
+}
