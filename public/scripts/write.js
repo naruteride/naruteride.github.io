b0618d7a392edff3dfc5c695411409cadec7d5cc
bangseonghun <slimejam01@gmail.com>
2021-08-22 22:40:44 +0900

    Update write.js

--- public/scripts/write.js
+++ public/scripts/write.js
@@ -4,9 +4,14 @@ import { bottomDrawerEventInit } from "./bottom-drawer.js";
 export function writeEventsInit() {
   new Promise((resolve, reject) => {
     bottomDrawer.innerHTML = writeHTML;
-		alert(window.writeLatLng)
+
     resolve();
   }).then(() => {
     bottomDrawerEventInit(bottomDrawer);
-  });
+  }).then(() => {
+		// 만약 글쓰기 페이지가 로드 되었는데 마커를 찍지 않았다면
+		if (window.writeLatLng = undefined) {
+			alert("지도를 꾹 눌러 생성하고픈 위치에 마커를 생성하세요!");
+		}
+	});
 }
