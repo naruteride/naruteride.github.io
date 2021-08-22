04f88ada3e45957cae48a64f71d029a40a10c4f1
bangseonghun <slimejam01@gmail.com>
2021-08-22 22:03:18 +0900

    Update write.js

--- public/scripts/write.js
+++ public/scripts/write.js
@@ -4,7 +4,7 @@ import { bottomDrawerEventInit } from "./bottom-drawer.js";
 export function writeEventsInit() {
   new Promise((resolve, reject) => {
     bottomDrawer.innerHTML = writeHTML;
-
+		console.log(window.writeLatLng)
     resolve();
   }).then(() => {
     bottomDrawerEventInit(bottomDrawer);
