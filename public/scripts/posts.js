d02e3db062696f292184e928cc5f3a30f68e0e7f
bangseonghun <slimejam01@gmail.com>
2021-08-25 23:55:09 +0900

    add logs

--- public/scripts/posts.js
+++ public/scripts/posts.js
@@ -10,25 +10,5 @@ export function postsEventsInit() {
     resolve();
   }).then(() => {
     bottomDrawerEventInit(bottomDrawer);
-
-    // 아래 코드는 주석처리 됨. 왜냐하면, 어짜피 a태그 클릭 시 해시값이 변경되어 index.js의 route() 함수가 postEventsInit()를 실행시켜줌
-    /**
-     * // 게시글 목록 클릭 시 발생하는 이벤트
-     * let postList = bottomDrawer.querySelectorAll(".list-group>a");
-     * for (let post of postList) {
-     * 
-     * // 게시글 클릭 시 게시글 페이지로 이동
-     *  post.addEventListener("click", function () {
-     *    postEventsInit();
-     * });
-     * }
-     * 
-     * // 글쓰기 버튼 클릭 시 발생하는 이벤트
-     * let writeButton = document.querySelector("#write");
-     * writeButton.addEventListener("click", () => {
-     *   writeEventsInit();
-     * });
-     */
-    
   });
 };
--- public/scripts/write.js
+++ public/scripts/write.js
@@ -4,15 +4,24 @@ import { bottomDrawerEventInit } from "./bottom-drawer.js";
 export function writeEventsInit() {
   new Promise((resolve, reject) => {
     bottomDrawer.innerHTML = writeHTML;
+    console.log("바텀 드로워에 write.html 넣음.");
 
     resolve();
-  }).then(() => {
-    bottomDrawerEventInit(bottomDrawer);
-  }).then(() => {
-    // 만약 글쓰기 페이지가 로드 되었는데 마커를 찍지 않았다면
-    console.log("바텀 드로워에 'write.html'넣은 뒤 바텀 드로워 초기화 함수까지 실행함.");
-		if (window.writeLatLng = undefined) {
-			alert("지도를 꾹 눌러 생성하고픈 위치에 마커를 생성하세요!");
-		}
-	});
+  })
+    .then(() => {
+      bottomDrawerEventInit(bottomDrawer);
+      console.log("바텀 드로워 초기화 함수 실행함.");
+    })
+    .then(() => {
+      // 만약 글쓰기 페이지가 로드 되었는데 마커를 찍지 않았다면
+      console.log(
+        "바텀 드로워에 'write.html'넣은 뒤 바텀 드로워 초기화 함수까지 실행함."
+      );
+      if ((window.writeLatLng = undefined)) {
+        alert("지도를 꾹 눌러 생성하고픈 위치에 마커를 생성하세요!");
+      }
+    })
+    .catch((err) => {
+      console.warn("writeEventsInit fetch went wrong.", err);
+    });
 }
