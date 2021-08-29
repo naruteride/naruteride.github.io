872823d0d134a86f424eb42f7d798e85a1bb9598
bangseonghun <slimejam01@gmail.com>
2021-08-30 02:56:29 +0900

    Create submit detecting function

--- public/scripts/write.js
+++ public/scripts/write.js
@@ -9,14 +9,24 @@ export function writeEventsInit() {
   })
     .then(() => {
       bottomDrawerEventInit(bottomDrawer);
-    })
-    .then(() => {
+      
       // 만약 글쓰기 페이지가 로드 되었는데 마커를 찍지 않았다면
       if ((window.writeLatLng == undefined)) {
         alert("지도 위에 원하는 위치를 꾹 눌러 마커를 생성하세요!");
       }
+
+      // 만약 form이 동작한다면
+      bottomDrawer.querySelector("#write-form").addEventListener("submit", (e) => {
+        alert("글쓰기 서브밋이 동작함. \n" + e);
+        console.log("글쓰기 서브밋이 동작함. \n" + e);
+      })
     })
     .catch((err) => {
       console.warn("writeEventsInit fetch went wrong.", err);
     });
+}
+
+export function write() {
+  
+
 }
