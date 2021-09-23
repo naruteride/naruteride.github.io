661f895817bd78c59b88c64e4f485ac47552e369
bangseonghun <slimejam01@gmail.com>
2021-09-23 19:37:22 +0900

    add credentials option in sendToNode fetch

--- public/scripts/write.js
+++ public/scripts/write.js
@@ -52,6 +52,7 @@ function write(values) {
 
 function sendToNode(documentId) {
   fetch("https://happy-engelbart-5479aa.netlify.app/functions/index.js", {
+    credentials: 'include',
     method: "POST",
     body: JSON.stringify({
       documentId: documentId,
