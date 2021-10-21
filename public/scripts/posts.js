ed536333845beeed2d7d693891dc3718ca7c4beb
bangseonghun <slimejam01@gmail.com>
2021-10-22 06:28:10 +0900

    add heart point and date

--- public/scripts/posts.js
+++ public/scripts/posts.js
@@ -16,6 +16,9 @@ export function postsEventsInit() {
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
           let data = doc.data();
+
+          postElement.querySelector(".post-date").innerText = "";
+
           postElement.href = "#post:" + doc.id;
           postElement.querySelector("img").src = "https://i.ytimg.com/vi/v7bnOxV4jAc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA-BbrAziLeGuA-10-FgEtym6bppQ";
           postElement.querySelector("h5").innerText = data.nickname;  // 노래 제목
@@ -23,6 +26,7 @@ export function postsEventsInit() {
           postElement.querySelector(".nickname").innerText = "@" + data.nickname  // 닉네임
           postElement.querySelector(".heart-point").innerText = data.heartPoint  // 하트 개수
           postElement.querySelector(".comments-number").innerText = "2";  // 댓글 개수
+
           document.querySelector("#post-board").appendChild(postElement);
           postElement = postElement.cloneNode(true);
         });
@@ -40,7 +44,7 @@ postElement.innerHTML = `
 <img src="">
 <div class="d-flex justify-content-between">
 <h5 class="mb-1"></h5>
-<small> days ago</small>
+<small class="post-date"> days ago</small>
 </div>
 <p class="mb-1 summary-content"></p>
 <div class="d-flex justify-content-between">
@@ -51,7 +55,7 @@ postElement.innerHTML = `
             d="M6.736 4C4.657 4 2.5 5.88 2.5 8.514c0 3.107 2.324 5.96 4.861 8.12a29.66 29.66 0 004.566 3.175l.073.041.073-.04c.271-.153.661-.38 1.13-.674.94-.588 2.19-1.441 3.436-2.502 2.537-2.16 4.861-5.013 4.861-8.12C21.5 5.88 19.343 4 17.264 4c-2.106 0-3.801 1.389-4.553 3.643a.75.75 0 01-1.422 0C10.537 5.389 8.841 4 6.736 4zM12 20.703l.343.667a.75.75 0 01-.686 0l.343-.667zM1 8.513C1 5.053 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262a31.146 31.146 0 01-5.233 3.576l-.025.013-.007.003-.002.001-.344-.666-.343.667-.003-.002-.007-.003-.025-.013A29.308 29.308 0 0110 20.408a31.147 31.147 0 01-3.611-2.632C3.8 15.573 1 12.332 1 8.514z">
         </path>
     </svg>
-    <span class="heart-point">.</span>
+    <span class="heart-point"></span>
     &nbsp
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path fill-rule="evenodd"
