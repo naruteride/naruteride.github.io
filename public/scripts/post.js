b8857e4a566a4d686e7d9eb2c759f474d5e3db28
bangseonghun <slimejam01@gmail.com>
2021-10-25 04:07:36 +0900

    Update post.js

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -18,8 +18,10 @@ export function postEventsInit() {
         bottomDrawer.querySelector(".title>b").innerText = data.nickname; // 곡 이름
         bottomDrawer.querySelector(".title>i").innerText = "- " + data.singer; // 가수
         bottomDrawer.querySelector(".nickname").innerText = "@" + data.nickname; // 작성자
+
         // bottomDrawer.querySelector(".post-date").innerText = toStringByFormatting(DateTime.parse(data.date.toDate().toString())); // 글 작성일
-        bottomDrawer.querySelector(".post-date").innerText = DateTime.parse(data.date.toDate().toString());
+        bottomDrawer.querySelector(".post-date").innerText = toStringByFormatting(data.date.toDate().toString());
+
         bottomDrawer.querySelector(".summary-content").innerText = data.content; // 게시글 내용
         bottomDrawer.querySelector(".content>p").innerText = data.content; // 게시글 내용
         bottomDrawer.querySelector(".heart-point").innerText = data.heartPoint; // 하트포인트
