7d29365b072afd2ece681a7fb31ec530e354bf52
bangseonghun <slimejam01@gmail.com>
2021-10-25 05:03:49 +0900

    fix a date method

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -18,9 +18,7 @@ export function postEventsInit() {
         bottomDrawer.querySelector(".title>b").innerText = data.nickname; // 곡 이름
         bottomDrawer.querySelector(".title>i").innerText = "- " + data.singer; // 가수
         bottomDrawer.querySelector(".nickname").innerText = "@" + data.nickname; // 작성자
-
-        bottomDrawer.querySelector(".post-date").innerText = timeConverter(data.date.toMillis());
-
+        bottomDrawer.querySelector(".post-date").innerText = timeConverter(data.date.toMillis()); // 작성 날짜
         bottomDrawer.querySelector(".summary-content").innerText = data.content; // 게시글 내용
         bottomDrawer.querySelector(".content>p").innerText = data.content; // 게시글 내용
         bottomDrawer.querySelector(".heart-point").innerText = data.heartPoint; // 하트포인트
