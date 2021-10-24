68f69c914e28c180c1f291b60aaf9a7be2c66d84
bangseonghun <slimejam01@gmail.com>
2021-10-25 03:06:07 +0900

    add a method to insert fetched data into post.html

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -9,11 +9,19 @@ export function postEventsInit() {
   }).then(() => {
     bottomDrawerEventInit(bottomDrawer);
 
-    
+    db.collection("posts")
+      .doc(location.hash.split(":")[1])
+      .get()
+      .then((doc) => {
+        let data = doc.data();
 
-    db.collection("posts").doc(location.hash.split(":")[1]).get().then((doc) => {
-      console.log(doc.data());
-      console.log(doc.data().nickname);
-    })
+        bottomDrawer.querySelector(".title>b").innerText = data.nickname; // 곡 이름
+        bottomDrawer.querySelector(".title>i").innerText = "- " + data.singer; // 가수
+        bottomDrawer.querySelector(".nickname").innerText = "@" + data.nickname; // 작성자
+        bottomDrawer.querySelector(".post-date").innerText = data.date; // 글 작성일
+        bottomDrawer.querySelector(".summary-content").innerText = data.content;  // 게시글 내용
+        bottomDrawer.querySelector(".content>p").innerText = data.content; // 게시글 내용
+        bottomDrawer.querySelector(".heart-point").innerText = data.heartPoint;
+      });
   });
 }
