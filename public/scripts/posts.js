34596986ade52390438e0b17e788049734bcc174
bangseonghun <slimejam01@gmail.com>
2021-10-27 05:11:17 +0900

    add a log

--- public/scripts/posts.js
+++ public/scripts/posts.js
@@ -21,30 +21,31 @@ export function postsEventsInit() {
           // 날짜
           let postDate = postElement.querySelector(".post-date");
           let nowDate = new Date();
+          console.log("미래" + nowDate + ", " + data.date);
           if (nowDate.setHours(24, 0, 0, 0) < data.date) { // 오늘 이후에 작성되었다면
             postDate.innerText = "Future?";
-            console.log("미래" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("미래" + nowDate + ", " + data.date);
           } else if (nowDate.setHours(-24, 0, 0, 0) < data.date) { // 오늘 작성되었다면
             postDate.innerText = "Today";
-            console.log("오늘" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("오늘" + nowDate + ", " + data.date);
           } else if (nowDate.setHours(-24, 0, 0, 0) < data.date) { // 어제 작성되었다면
             postDate.innerText = "1 days ago";
-            console.log("어제" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("어제" + nowDate + ", " + data.date);
           } else if (nowDate.setHours(-24, 0, 0, 0) < data.date) { // 그제 작성되었다면
             postDate.innerText = "2 days ago";
-            console.log("그제" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("그제" + nowDate + ", " + data.date);
           } else if (nowDate.setHours(-24, 0, 0, 0) < data.date) { // 3일 전에 작성되었다면
             postDate.innerText = "3 days ago";
-            console.log("그끄저께 전" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("그끄저께 전" + nowDate + ", " + data.date);
           } else if (nowDate.setHours(-24, 0, 0, 0) < data.date) { // 4일 전에 작성되었다면
             postDate.innerText = "4 days ago";
-            console.log("나흘 전" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("나흘 전" + nowDate + ", " + data.date);
           } else if (nowDate.setHours(-24, 0, 0, 0) < data.date) { // 5일 전에 작성되었다면
             postDate.innerText = "5 days ago";
-            console.log("닷세 전" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("닷세 전" + nowDate + ", " + data.date);
           } else if (nowDate.setHours(-24, 0, 0, 0) < data.date) { // 6일 전에 작성되었다면
             postDate.innerText = "6 days ago";
-            console.log("엿세 전" + nowDate.setHours(24, 0, 0, 0) + ", " + data.date);
+            console.log("엿세 전" + nowDate + ", " + data.date);
           } else {
             postDate.innerText = timeConverter(data.date.toMillis());
           }
