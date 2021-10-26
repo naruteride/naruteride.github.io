f46e117e26d48cfd238f269da01977b783ce9688
bangseonghun <slimejam01@gmail.com>
2021-10-27 05:50:07 +0900

    Update posts.js

--- public/scripts/posts.js
+++ public/scripts/posts.js
@@ -25,25 +25,25 @@ export function postsEventsInit() {
           if (new Date().setUTCHours(24, 0, 0, 0) < data.date) { // 오늘 이후에 작성되었다면
             postDate.innerText = "Future?";
             console.log("미래: " + new Date() + ", " + data.date);
-          } else if (new Date().setUTCHours(-24, 0, 0, 0) < data.date) { // 오늘 작성되었다면
+          } else if (new Date().setUTCHours(0, 0, 0, 0) < data.date) { // 오늘 작성되었다면
             postDate.innerText = "Today";
             console.log("오늘: " + new Date() + ", " + data.date);
           } else if (new Date().setUTCHours(-24, 0, 0, 0) < data.date) { // 어제 작성되었다면
             postDate.innerText = "1 days ago";
             console.log("어제: " + new Date() + ", " + data.date);
-          } else if (new Date().setUTCHours(-24, 0, 0, 0) < data.date) { // 그제 작성되었다면
+          } else if (new Date().setUTCHours(-48, 0, 0, 0) < data.date) { // 그제 작성되었다면
             postDate.innerText = "2 days ago";
             console.log("그제: " + new Date() + ", " + data.date);
-          } else if (new Date().setUTCHours(-24, 0, 0, 0) < data.date) { // 3일 전에 작성되었다면
+          } else if (new Date().setUTCHours(-72, 0, 0, 0) < data.date) { // 3일 전에 작성되었다면
             postDate.innerText = "3 days ago";
             console.log("그끄저께: " + new Date() + ", " + data.date);
-          } else if (new Date().setUTCHours(-24, 0, 0, 0) < data.date) { // 4일 전에 작성되었다면
+          } else if (new Date().setUTCHours(-96, 0, 0, 0) < data.date) { // 4일 전에 작성되었다면
             postDate.innerText = "4 days ago";
             console.log("나흘 전: " + new Date() + ", " + data.date);
-          } else if (new Date().setUTCHours(-24, 0, 0, 0) < data.date) { // 5일 전에 작성되었다면
+          } else if (new Date().setUTCHours(-120, 0, 0, 0) < data.date) { // 5일 전에 작성되었다면
             postDate.innerText = "5 days ago";
             console.log("닷세 전: " + new Date() + ", " + data.date);
-          } else if (new Date().setUTCHours(-24, 0, 0, 0) < data.date) { // 6일 전에 작성되었다면
+          } else if (new Date().setUTCHours(-144, 0, 0, 0) < data.date) { // 6일 전에 작성되었다면
             postDate.innerText = "6 days ago";
             console.log("엿세 전: " + new Date() + ", " + data.date);
           } else {
