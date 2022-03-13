5895583d46f9ef07191828873014ffb83c044f36
bangseonghun <slimejam01@gmail.com>
2022-03-13 18:13:24 +0900

    modify submit to onclick in sign up

--- public/scripts/logins/signup.js
+++ public/scripts/logins/signup.js
@@ -1,37 +1,92 @@
-// 만약 글쓰기 form의 submit이 동작한다면, 해당 form에 formdata이벤트를 발생시킴
-let signUpForm = bottomDrawer.querySelector("#SignUp .form-box");
-signUpForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+// // 만약 글쓰기 form의 submit이 동작한다면, 해당 form에 formdata이벤트를 발생시킴
+// let signUpForm = bottomDrawer.querySelector("#SignUp .form-box");
+// signUpForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  new FormData(signUpForm);
-});
+//   new FormData(signUpForm);
+// });
 
-// 만약 회원가입 form의 formdata 이벤트가 발생한다면,
-signUpForm.addEventListener("formdata", (e) => {
-  signUp(e.formData);
-});
+// // 만약 회원가입 form의 formdata 이벤트가 발생한다면,
+// signUpForm.addEventListener("formdata", (e) => {
+//   signUp(e.formData);
+// });
+
+// // 회원가입 함수
+// function signUp(values) {
+//   document.querySelector("#loading-cover").classList.add("on"); // 로딩창
+
+//   console.log("사랑해~~~");
+//   console.log(JSON.stringify({
+//     email: values.get("email"),
+//     nickname: values.get("nickname"),
+//     password: values.get("password"),
+//     is_agreed: values.get("is_agreed"),
+//   }))
+
+//   fetch(
+//     "http://ec2-3-37-203-162.ap-northeast-2.compute.amazonaws.com:5468/auth/signup",
+//     {
+//       method: "POST",
+//       body: JSON.stringify({
+//         email: values.get("email"),
+//         nickname: values.get("nickname"),
+//         password: values.get("password"),
+//         is_agreed: values.get("is_agreed"),
+//       }),
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//       },
+//     }
+//   ).then((response) => {
+//     response.json().then((resData) => {
+//       if (response.status == 200) {
+//         // 만약 회원가입에 성공한다면
+//         setTimeout(() => {
+//           document.querySelector("#loading-cover").classList.remove("on"); // 로딩창 끔
+//           onSnackbar(resData.message); // 토스트
+//         }, 2000);
+//       } else {
+//         onSnackbar("Error");
+//       }
+//     });
+//   });
+// }
 
 // 회원가입 함수
 function signUp(values) {
   document.querySelector("#loading-cover").classList.add("on"); // 로딩창
 
   console.log("사랑해~~~");
-  console.log(JSON.stringify({
-    email: values.get("email"),
-    nickname: values.get("nickname"),
-    password: values.get("password"),
-    is_agreed: values.get("is_agreed"),
-  }))
+  console.log(
+    JSON.stringify({
+      email: document.querySelector("#SignUp .form-box input[name=email]")
+        .value,
+      nickname: document.querySelector("#SignUp .form-box input[name=nickname]")
+        .value,
+      password: document.querySelector("#SignUp .form-box input[name=password]")
+        .value,
+      is_agreed: document.querySelector(
+        "#SignUp .form-box input[name=is_agreed]"
+      ).value,
+    })
+  );
 
   fetch(
     "http://ec2-3-37-203-162.ap-northeast-2.compute.amazonaws.com:5468/auth/signup",
     {
       method: "POST",
       body: JSON.stringify({
-        email: values.get("email"),
-        nickname: values.get("nickname"),
-        password: values.get("password"),
-        is_agreed: values.get("is_agreed"),
+        email: document.querySelector("#SignUp .form-box input[name=email]")
+          .value,
+        nickname: document.querySelector(
+          "#SignUp .form-box input[name=nickname]"
+        ).value,
+        password: document.querySelector(
+          "#SignUp .form-box input[name=password]"
+        ).value,
+        is_agreed: document.querySelector(
+          "#SignUp .form-box input[name=is_agreed]"
+        ).value,
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
