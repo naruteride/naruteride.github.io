7691e6d095ffaf5c42a208d779572a74302c8321
bangseonghun <slimejam01@gmail.com>
2022-04-17 21:32:18 +0900

    add logs

--- public/scripts/logins/login.js
+++ public/scripts/logins/login.js
@@ -1,10 +1,15 @@
 let spotifyButton;
+let spotifyButton2 = document.querySelector("#spotify2");
+
 
 onload = () => {
     alert("온로드 실행됨");
     spotifyButton = document.querySelector("#spotify");
+    spotifyButton.addEventListener("click", () => {
+        alert("버튼 눌림");
+    })
 }
 
-spotifyButton.addEventListener("click", () => {
-    alert("버튼 눌림");
+spotifyButton2.addEventListener("click", () => {
+    alert("버튼 눌림2");
 })
