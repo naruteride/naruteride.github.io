d74510b6d954c06e771d9947e74183b5c41d4185
bangseonghun <slimejam01@gmail.com>
2022-04-17 21:57:42 +0900

    fix a bug

--- public/scripts/logins/login.js
+++ public/scripts/logins/login.js
@@ -1,7 +1,8 @@
 let spotifyButton;
+let state = Math.floor(Math.random() * Math.pow(10, 16));
 onload = () => {
     spotifyButton = document.querySelector("#spotify-login");
     spotifyButton.addEventListener("click", () => {
-        location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=3df833a583c149188b0c193c67b9731b&scope=user-read-private%20user-read-email&redirect_uri=https://naruteride.github.io/views/&state=" + generateRandomString(16);
+        location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=3df833a583c149188b0c193c67b9731b&scope=user-read-private%20user-read-email&redirect_uri=https://naruteride.github.io/views/&state=" + state;
     })
 }
