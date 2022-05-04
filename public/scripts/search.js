bff20b9d1b4cfe706c65439b3eb7a3caba031d83
bangseonghun <slimejam01@gmail.com>
2022-05-04 10:40:15 +0900

    Add a method to change color of search option buttons

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,18 +1,40 @@
 let cardList;
 let searchInput, searchIcon;
+let searchOptionTag, searchOptionUser, searchOptionSong;
 
 onload = () => {
     cardList = document.querySelector("#card-list");
     fetchCard();
 
+	// 검색 바
     searchInput = document.querySelector("#search-bar>input[type=search]");
     searchIcon = document.querySelector("#search-bar>button>img");
     searchInput.addEventListener("focus", (event) => {
         searchIcon.classList.remove("off");
-    }, true);
+    });
     searchInput.addEventListener("blur", (event) => {
         searchIcon.classList.add("off");
-    }, true);
+    });
+
+	// 검색 옵션 버튼들
+	searchOptionTag = document.querySelector("#search-option>button.tag");
+	searchOptionUser = document.querySelector("#search-option>button.user");
+	searchOptionSong = document.querySelector("#search-option>button.song");
+	searchOptionTag.addEventListener("click", (event) => {
+		searchOptionTag.classList.add("on");
+		searchOptionUser.classList.remove("on");
+		searchOptionSong.classList.remove("on");
+	});
+	searchOptionUser.addEventListener("click", (event) => {
+		searchOptionTag.classList.remove("on");
+		searchOptionUser.classList.add("on");
+		searchOptionSong.classList.remove("on");
+	});
+	searchOptionSong.addEventListener("click", (event) => {
+		searchOptionTag.classList.remove("on");
+		searchOptionUser.classList.remove("on");
+		searchOptionSong.classList.add("on");
+	});
 }
 
 function fetchCard() {
