d76d060c692799f9416bdf45960125eb459182ae
bangseonghun <slimejam01@gmail.com>
2022-05-04 10:11:48 +0900

    Add a method to change search icon color

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,24 +1,34 @@
 let cardList;
+let searchInput, searchIcon;
 
 onload = () => {
     cardList = document.querySelector("#card-list");
     fetchCard();
+
+    searchInput = document.querySelector("#search-bar>input[type=search]");
+    searchIcon = document.querySelector("#search-icon>button>img");
+    searchInput.addEventListener("focus", (event) => {
+        searchIcon.classList.remove("off");
+    }, true);
+    searchInput.addEventListener("blur", (event) => {
+        searchIcon.classList.add("off");
+    }, true);
 }
 
 function fetchCard() {
     return fetch("/views/card.html")
-    .then((response) => {
-        return response.text();
-    })
-    .then((html) => {
-        for (let i = 0; i < 5; i++) {
-            cardList.insertAdjacentHTML("beforeend", html);
-        }
-        document.querySelector(".digging").addEventListener("click", () => {
-            // alert("digging");
+        .then((response) => {
+            return response.text();
         })
-    })
-    .catch((err) => {
-        console.error("Card list fetch went wrong.", err);
-    })
+        .then((html) => {
+            for (let i = 0; i < 5; i++) {
+                cardList.insertAdjacentHTML("beforeend", html);
+            }
+            document.querySelector(".digging").addEventListener("click", () => {
+                // alert("digging");
+            })
+        })
+        .catch((err) => {
+            console.error("Card list fetch went wrong.", err);
+        })
 }
