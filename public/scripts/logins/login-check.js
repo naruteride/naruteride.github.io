427a4a2ce9906fdb3af14bcb94eeaeba78532bad
bangseonghun <slimejam01@gmail.com>
2022-05-07 20:30:03 +0900

    Add a mode

--- public/scripts/logins/login-check.js
+++ public/scripts/logins/login-check.js
@@ -3,6 +3,7 @@ const url = new URL(urlStr);
 
 fetch("http://ec2-3-37-203-162.ap-northeast-2.compute.amazonaws.com:5468/auth/signup/social", {
     method: "POST",
+    mode: "no-cors",
     body: JSON.stringify({
         code: url.searchParams.get("code"),
         redirect_uri: "http://localhost:3000/views/logins/login-check",
