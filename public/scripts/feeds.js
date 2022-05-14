353db75b36ed59039c369814cbd18cd016480bb4
bangseonghun <slimejam01@gmail.com>
2022-05-14 20:23:09 +0900

    Create a spotify player

--- public/scripts/feeds.js
+++ public/scripts/feeds.js
@@ -1,24 +1,24 @@
 let cardList;
 
 onload = () => {
-    cardList = document.querySelector("#card-list");
-    fetchCard();
+	cardList = document.querySelector("#card-list");
+	fetchCard();
 }
 
 function fetchCard() {
-    return fetch("/views/card.html")
-    .then((response) => {
-        return response.text();
-    })
-    .then((html) => {
-        for (let i = 0; i < 5; i++) {
-            cardList.insertAdjacentHTML("beforeend", html);
-        }
-        document.querySelector(".digging").addEventListener("click", () => {
-            // alert("digging");
-        })
-    })
-    .catch((err) => {
-        console.error("Card list fetch went wrong.", err);
-    })
+	return fetch("/views/card.html")
+		.then((response) => {
+			return response.text();
+		})
+		.then((html) => {
+			for (let i = 0; i < 5; i++) {
+				cardList.insertAdjacentHTML("beforeend", html);
+			}
+			document.querySelector(".digging").addEventListener("click", () => {
+				// alert("digging");
+			})
+		})
+		.catch((err) => {
+			console.error("Card list fetch went wrong.", err);
+		})
 }
