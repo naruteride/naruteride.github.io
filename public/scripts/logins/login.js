574acdfd5152180a022d9925411e4c749232384f
bangseonghun <slimejam01@gmail.com>
2022-05-15 17:12:26 +0900

    Add a guest login button & Change a logo image & Remove some comments

--- public/scripts/logins/login.js
+++ public/scripts/logins/login.js
@@ -1,4 +1,4 @@
-let spotifyButton;
+let spotifyButton, guestButton;
 let state = Math.floor(Math.random() * Math.pow(10, 16)).toString();
 while (state.length < 16) {
 	state = '0' + state;
@@ -7,5 +7,9 @@ onload = () => {
 	spotifyButton = document.querySelector("#spotify-login");
 	spotifyButton.addEventListener("click", () => {
 		location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=965724cc721a4fa3b21ec344ae31bf12&scope=user-read-private%20user-read-email&redirect_uri=http://localhost:3000/views/logins/login-check&state=" + state;
+	})
+	guestButton = document.querySelector("#guest-login");
+	guestButton.addEventListener("click", () => {
+		location.href = "/views/feeds";
 	})
 }
