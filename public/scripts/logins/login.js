1b50cb8f6c4f5693985cd2a7d000fb1472f47217
bangseonghun <slimejam01@gmail.com>
2022-05-21 05:06:48 +0900

    Update login.js

--- public/scripts/logins/login.js
+++ public/scripts/logins/login.js
@@ -10,7 +10,7 @@ onload = () => {
 	});
 	spotifyButton = document.querySelector("#spotify-login");
 	spotifyButton.addEventListener("click", () => {
-		onSnackbar("스포티파이 로그인은 아직 지원하지 않습니다!")
+		onSnackbar("스포티파이 로그인 업데이트 예정")
 		// location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=965724cc721a4fa3b21ec344ae31bf12&scope=user-read-private%20user-read-email&redirect_uri=" + window.location.protocol + "//" + window.location.host + "/views/logins/login-check&state=" + state;
 	});
 	snackbar = document.querySelector("#snackbar");
