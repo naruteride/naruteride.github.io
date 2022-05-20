0c288c707d7251d6653e9b94607ef118ae921cdc
bangseonghun <slimejam01@gmail.com>
2022-05-21 02:12:03 +0900

    Deactivation a spotify login

--- public/scripts/logins/login.js
+++ public/scripts/logins/login.js
@@ -6,10 +6,22 @@ while (state.length < 16) {
 onload = () => {
 	spotifyButton = document.querySelector("#spotify-login");
 	spotifyButton.addEventListener("click", () => {
-		location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=965724cc721a4fa3b21ec344ae31bf12&scope=user-read-private%20user-read-email&redirect_uri=" + window.location.protocol + "//" + window.location.host + "/views/logins/login-check&state=" + state;
+		onSnackbar("스포티파이 로그인은 아직 지원하지 않습니다!")
+		// location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=965724cc721a4fa3b21ec344ae31bf12&scope=user-read-private%20user-read-email&redirect_uri=" + window.location.protocol + "//" + window.location.host + "/views/logins/login-check&state=" + state;
 	})
 	guestButton = document.querySelector("#guest-login");
 	guestButton.addEventListener("click", () => {
 		location.href = "/views/logins/login-check#guest";
 	})
+}
+
+// 스낵바 함수
+let snackbar;
+function onSnackbar(message) {
+	snackbar.innerText = message;
+	snackbar.classList.add("show");
+
+	setTimeout(() => {
+		snackbar.classList.remove("show");
+	}, 3000);
 }
