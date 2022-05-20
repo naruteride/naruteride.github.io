d765f19dadb047fd06b7983444bf83090854a598
bangseonghun <slimejam01@gmail.com>
2022-05-21 05:00:19 +0900

    Fix a Snackbar bug in login page

--- public/scripts/logins/login.js
+++ public/scripts/logins/login.js
@@ -4,15 +4,16 @@ while (state.length < 16) {
 	state = '0' + state;
 }
 onload = () => {
+	guestButton = document.querySelector("#guest-login");
+	guestButton.addEventListener("click", () => {
+		location.href = "/views/logins/login-check#guest";
+	});
 	spotifyButton = document.querySelector("#spotify-login");
 	spotifyButton.addEventListener("click", () => {
 		onSnackbar("스포티파이 로그인은 아직 지원하지 않습니다!")
 		// location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=965724cc721a4fa3b21ec344ae31bf12&scope=user-read-private%20user-read-email&redirect_uri=" + window.location.protocol + "//" + window.location.host + "/views/logins/login-check&state=" + state;
-	})
-	guestButton = document.querySelector("#guest-login");
-	guestButton.addEventListener("click", () => {
-		location.href = "/views/logins/login-check#guest";
-	})
+	});
+	snackbar = document.querySelector("#snackbar");
 }
 
 // 스낵바 함수
